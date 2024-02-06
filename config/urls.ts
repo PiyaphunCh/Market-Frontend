756fef036e28a89c656ba24157c22e607600f4ac
PiyaphunCh <137492968+PiyaphunCh@users.noreply.github.com>
2024-02-06 10:34:31 +0700

    change url

--- config/urls.ts
+++ config/urls.ts
@@ -1,4 +1,4 @@
 export const urls = {
-  getItemRol: `http://localhost:3000/api/get/item/set/price`,
-  updateItemRol: `http://localhost:3000/api/update/item/set/price`
+  getItemRol: `https://market-backend-three.vercel.app/api/get/item/set/price`,
+  updateItemRol: `https://market-backend-three.vercel.app/api/update/item/set/price`
 }
